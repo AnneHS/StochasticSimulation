0775fb42e991b06f3f52e3a1ed0dbfc7bd2e5caa
annehs <annehoogerduijn@gmail.com>
2020-11-29 18:09:06 +0100

    test.py voor meerdere rho's

---
+++ Assignment2/code/plot.py
@@ -0,0 +1,42 @@
+import sys
+import csv
+import matplotlib.pyplot as plt
+import os
+import pandas as pd
+import numpy as np
+
+fileName = '../data/mm2_results.csv'
+df = pd.read_csv(fileName)
+df.columns = ['rho', 'waiting_time', 'lenght_queue']
+rhos = [0.05, 0.1, 0.15, 0.20, 0.25, 0.30, 0.35, 0.40, 0.45, 0.50, 0.55, 0.60, 0.65, 0.7, 0.75, 0.8, 0.85, 0.90, 0.95]
+
+means=df.groupby('rho')['waiting_time'].mean()
+stdevs=df.groupby('rho')['waiting_time'].std()
+print(means)
+plt.plot(means, label='M/M/2')
+plt.fill_between(rhos, means-stdevs,means+stdevs,alpha=.1,)
+plt.legend()
+plt.xlabel('Rho')
+plt.ylabel('Average waiting time')
+plt.show()
+'''
+print('jo')
+means=[]
+stdevs=[]
+for rho in rhos:
+    print(rho)
+    mean = df[(df['rho']==rho)]['waiting_time'].mean()
+    stdev = df[(df['rho']==rho)]['waiting_time'].std()
+
+    means.append(mean)
+    stdevs.append(stdevs)
+
+print('jojo')
+#means = np.asarray(means)
+#stdevs = np.asarray(stdevs)
+plt.plot(rhos, means)
+plt.fill_between(rhos, means-stdevs,means+stdevs,alpha=.1)
+plt.xlabel('rho')
+plt.ylabel('waiting time')
+plt.show()
+'''
--- Assignment2/code/test.py
+++ Assignment2/code/test.py
@@ -14,7 +14,7 @@ class Customer():
         self.name = name
         self.joblength = l
 
-    def request(self, env, server, mu):
+    def request(self, env, server, mu, n):
         arrival = env.now
         in_queue.append(self.name)
         print('%7.4f %s: Here I am' % (arrival, self.name))
@@ -32,10 +32,10 @@ class Customer():
             # Time until departure
             tib = random.expovariate(mu) #  time until departure
             yield env.timeout(tib) # wait until server is finished
-            print('%7.4f %s: Finished' % (env.now, self.name))
+            #print('%7.4f %s: Finished' % (env.now, self.name))
 
             # File for temp results
-            file_name = "../data/mm" + str(mu)  + "_temp.csv"
+            file_name = "../data/mm" + str(n)  + "_temp.csv"
             if not os.path.isfile(file_name):
                     open(file_name, 'x')
             # Save results
@@ -44,7 +44,7 @@ class Customer():
                 writer.writerow([arrival, waiting_time, len(in_queue)])
 
 
-def source(env, server, l, mu):
+def source(env, server, l, mu, n):
     """Source generates customers randomly"""
 
     i = 1 #customer counter
@@ -52,20 +52,20 @@ def source(env, server, l, mu):
     while True:
         customer = Customer(env, "Customer%02d"%(i), l)
         i += 1
-        env.process(customer.request(env, server, mu)) #customer arrives
+        env.process(customer.request(env, server, mu, n)) #customer arrives
         t = random.expovariate(l) #arrival time of the next customer
         yield env.timeout(t) #timeout until arrival time of next customer
 
 
-def  save_means(mu, nr_of_batches, t):
+def save_means(n, nr_of_batches, t, rho):
 
     # Open temp results
-    results_file = "../data/mm" + str(mu)  + "_temp.csv"
-    df = pd.read_csv(results_file)
+    temp_results = "../data/mm" + str(n)  + "_temp.csv"
+    df = pd.read_csv(temp_results)
     df.columns=["arrival", "waiting_time", "len_queue"]
 
     # Results file
-    file_name = "../data/mm" + str(mu)  + "_results.csv"
+    file_name = "../data/mm" + str(n)  + "_results.csv"
     if not os.path.isfile(file_name):
             open(file_name, 'x')
 
@@ -79,34 +79,40 @@ def  save_means(mu, nr_of_batches, t):
         with open(file_name, 'a') as resultsFile:
             writer = csv.writer(resultsFile)
             writer.writerow([
+                rho,
                 df[(df["arrival"] >= i * batch_size) & (df["arrival"] < (i+1) * batch_size)]["waiting_time"].mean(),
                 df[(df["arrival"] >= i * batch_size) & (df["arrival"] < (i+1) * batch_size)]["len_queue"].mean()
             ])
 
+    # Remove temp results (new temp for new rho)
+    os.remove(temp_results)
 
 def main():
-    #variables
     mu = 1 #capacity of each of n equal servers
-    n = 1 #amount of servers
-    rho = 0.9 #system load
-    l = rho*mu*(1/n) #arrival rate into the whole system (lambda)
-    t = 20 #end timee
+    n = 2 #amount of servers
+    t = 10000 #end timee
     RANDOM_SEED = 2
 
-    if rho >= 1: #rho cant be larger than 1, raise error, to ensure queue is stable
-        raise Exception("rho can't be 1 or larger, rho = ", rho)
+    rhos = [0.05, 0.1, 0.15, 0.20, 0.25, 0.30, 0.35, 0.40, 0.45, 0.50, 0.55, 0.60, 0.65, 0.7, 0.75, 0.8, 0.85, 0.90, 0.95] # System load
+    for rho in rhos:
+        #variables
+        l = rho*mu*(1/n) #arrival rate into the whole system (lambda)
 
-    # Setup and start the simulation
-    print('Start of FIFO M/M/n, lambda = ', l, ", mu = ", mu, ", n = ", n, "rho = ", rho )
-    random.seed(RANDOM_SEED)
-    env = sp.Environment()
-    server = sp.Resource(env, capacity=n)
-    env.process(source(env, server, l, mu))
-    env.run(until = t)
 
-    # Save results per batch
-    nr_of_batches=4
-    save_means(mu, nr_of_batches, t)
+        if rho >= 1: #rho cant be larger than 1, raise error, to ensure queue is stable
+            raise Exception("rho can't be 1 or larger, rho = ", rho)
+
+        # Setup and start the simulation
+        print('Start of FIFO M/M/n, lambda = ', l, ", mu = ", mu, ", n = ", n, "rho = ", rho )
+        random.seed(RANDOM_SEED)
+        env = sp.Environment()
+        server = sp.Resource(env, capacity=n)
+        env.process(source(env, server, l, mu, n))
+        env.run(until = t)
+
+        # Save results per batch
+        nr_of_batches=20
+        save_means(n, nr_of_batches, t, rho)
 
 if __name__ == "__main__":
     main()
