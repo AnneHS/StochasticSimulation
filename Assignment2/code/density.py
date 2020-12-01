10a0a66744b1a81061a4f29323f99368d642064b
marije <marije.dekker@hotmail.com>
2020-12-01 19:54:43 +0100

    final thingies

--- Assignment2/code/density.py
+++ Assignment2/code/density.py
@@ -27,21 +27,21 @@ def main():
 
             # For each unique rho value
             #rhos = df.rho.unique().tolist()
-            rhos=[0.9]
+            rhos=[0.1]
 
             for rho in rhos:
                 this_data = df.loc[df['rho'] == rho]
                 #print(this_data["waiting_time"])
                 #sns.distplot(waiting_time, norm_hist=True)#, hist=True, kde=False)
                 sns.kdeplot(this_data["waiting_time"],shade=True)
-    title = "density plot of waiting times for rho = 0.9"
+    title = "density plot of waiting times for rho = 0.1"
     plt.title(title)
     plt.xlabel("waiting time")
     plt.ylabel("density")
     plt.xlim([0,None])
     plt.ylim([0,None])
     plt.legend(["M/M/1", "M/M/2","M/M/4","M/M/1, priority", "M/H/1","M/D/1"])
-    plt.savefig("../plots/density_rho=0.9.jpg", dpi=300)
+    #plt.savefig("../plots/density_rho=0.9.jpg", dpi=300)
     plt.show()
 
 
